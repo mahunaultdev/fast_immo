9fee7dbd5cb7b63178804786b8eae95ee4dec751
Hydred <clement.boufflet@gmail.com>
2021-01-22 18:19:34 +0100

    dossier avec template 1.0

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ Encore
      * ENTRY CONFIG
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
 
@@ -56,7 +56,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -69,7 +69,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 
     .copyFiles({
         from: './assets/images',
@@ -86,6 +86,15 @@ Encore
         */   
     })
 ;
+const webpack = require('webpack');
+
+plugins: [
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+    }),
+];
 
 module.exports = Encore.getWebpackConfig();
 
